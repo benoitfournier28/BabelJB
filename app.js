05adb264d561ca9c2a180335e38c74bac34813d7
Benoît Fournier <benoitfournier28pro@gmail.com>
2020-07-21 14:02:29 +0200

    modif html pour header et footer

---
+++ app.js
@@ -0,0 +1,8 @@
+const btnChangeASho = document.querySelector("#pageASho");
+const btnChangeBSho = document.querySelector("#pageBSho");
+
+// btnChangeBSho.addEventListener('click',function(){
+//     if("btnChangeBSho"=true){
+//         URL
+//     }
+// });
